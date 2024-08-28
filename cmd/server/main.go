18b040b4a423aa61643a28db918ef93ae8807e85
Chara-X <1719552189@qq.com>
2024-08-29 02:41:53 +0800

    Up

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,13 +8,20 @@ import (
 	"os"
 	"os/exec"
 
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	core "k8s.io/api/core/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/kubernetes/pkg/apis/apidiscovery"
-	"k8s.io/kubernetes/pkg/apis/core"
 )
 
+var cmds = map[string]*exec.Cmd{}
+
 func main() {
+	defer func() {
+		for _, cmd := range cmds {
+			cmd.Process.Kill()
+		}
+	}()
 	var mux = http.NewServeMux()
 	mux.HandleFunc("GET /apis", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json;g=apidiscovery.k8s.io;v=v2;as=APIGroupDiscoveryList")
@@ -24,21 +31,47 @@ func main() {
 		json.NewEncoder(w).Encode(apis)
 	})
 	mux.HandleFunc("POST /api/v1/namespaces/default/pods", func(w http.ResponseWriter, r *http.Request) {
-		var pod = &core.Pod{}
-		json.NewDecoder(r.Body).Decode(pod)
-		exec.Command(pod.Spec.Containers[0].Image).Start()
-		pod.Status.Phase = core.PodRunning
+		var pod = core.Pod{}
+		json.NewDecoder(r.Body).Decode(&pod)
+		var cmd = exec.Command(pod.Spec.Containers[0].Image)
+		cmd.Start()
+		cmds[pod.Name] = cmd
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(pod)
+		json.NewEncoder(w).Encode(toPod(pod.Name, cmd))
 	})
 	mux.HandleFunc("PUT /api/v1/namespaces/default/pods/{name}", func(w http.ResponseWriter, r *http.Request) {})
 	mux.HandleFunc("DELETE /api/v1/namespaces/default/pods/{name}", func(w http.ResponseWriter, r *http.Request) {})
-	mux.HandleFunc("GET /api/v1/namespaces/default/pods", func(w http.ResponseWriter, r *http.Request) {
-		var pods = &core.PodList{TypeMeta: v1.TypeMeta{APIVersion: "v1", Kind: "PodList"}}
+	mux.HandleFunc("GET /api/v1/namespaces/default/pods/{name}", func(w http.ResponseWriter, r *http.Request) {
+		var cmd, ok = cmds[r.PathValue("name")]
 		w.Header().Set("Content-Type", "application/json")
+		if !ok {
+			var status = meta.Status{Reason: "NotFound", Message: "Pod not found", Code: http.StatusNotFound}
+			w.WriteHeader(http.StatusNotFound)
+			json.NewEncoder(w).Encode(status)
+		} else {
+			json.NewEncoder(w).Encode(toPod(r.PathValue("name"), cmd))
+		}
+	})
+	mux.HandleFunc("GET /api/v1/namespaces/default/pods", func(w http.ResponseWriter, r *http.Request) {
+		var pods = &core.PodList{TypeMeta: meta.TypeMeta{APIVersion: "v1", Kind: "PodList"}}
+		w.Header().Set("Content-Type", "application/json")
+		for k, v := range cmds {
+			pods.Items = append(pods.Items, toPod(k, v))
+		}
 		json.NewEncoder(w).Encode(pods)
 	})
 	http.ListenAndServe("127.0.0.1:6443", &logsMiddleware{next: mux})
+}
+
+func toPod(name string, cmd *exec.Cmd) core.Pod {
+	return core.Pod{
+		TypeMeta:   meta.TypeMeta{APIVersion: "v1", Kind: "Pod"},
+		ObjectMeta: meta.ObjectMeta{Name: name, Namespace: "default"},
+		Spec: core.PodSpec{
+			Containers: []core.Container{{Name: name, Image: cmd.Path}},
+		},
+		Status: core.PodStatus{Phase: core.PodPhase(cmd.ProcessState.String())},
+	}
 }
 
 type logsMiddleware struct{ next http.Handler }
