45ee569fa775833ae3c14b31ab3f2c2b8e7b1d9a
Chara-X <1719552189@qq.com>
2024-09-13 21:26:29 +0800

    Up

--- cmd/kube/init.go
+++ cmd/kube/init.go
@@ -4,7 +4,6 @@ import (
 	"os"
 	"sync"
 
-	core "k8s.io/api/core/v1"
 	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/yaml"
 	"k8s.io/kubernetes/pkg/apis/apidiscovery"
@@ -17,12 +16,4 @@ var apis = &apidiscovery.APIGroupDiscoveryList{}
 func init() {
 	var apisData, _ = os.ReadFile("./apis.yaml")
 	yaml.Unmarshal(apisData, apis)
-	var node = &core.Node{
-		TypeMeta:   meta.TypeMeta{APIVersion: "v1", Kind: "Node"},
-		ObjectMeta: meta.ObjectMeta{Name: "kube"},
-		Spec:       core.NodeSpec{},
-		Status:     core.NodeStatus{},
-	}
-	node.SetCreationTimestamp(meta.Now())
-	ctx.Store("kube", node)
 }
